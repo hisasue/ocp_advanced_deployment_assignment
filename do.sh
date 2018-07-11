dd33e002a3590477c31e04fef40e98b5c949575d
root <root@bastion.fd64.example.opentlc.com>
2018-07-11 02:05:33 +0000

    add and remove files for setting up local environment

--- do.sh
+++ do.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ansible-playbook local.yml
+ansible-playbook -i hosts pre.yml
 ansible-playbook -f 20 -i hosts /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -f 20 -i hosts /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
 ansible-playbook -i hosts site.yml
